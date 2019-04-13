f9191f1f84a10627c65507d8bb0ffca1e703f1e4
Yaw Owusu-Ansah <jyoansah@gmail.com>
2019-04-13 02:39:18 -0400

    update composer.lock

--- public/js/app.js
+++ public/js/app.js
@@ -37454,7 +37454,6 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 				var data = _ref.data;
 
 				_this.organization = data.data;
-				console.log("Function: , Line 65 this.organization (): ", _this.organization);
 			}).catch(function (error) {
 				toastr["error"](error.response.data.message);
 			});
